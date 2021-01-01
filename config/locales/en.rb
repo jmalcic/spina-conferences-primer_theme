2173e997015d4d38329527c4101667cb6f41b036
Louis Van Steene <louisvansteene@gmail.com>
2021-01-01 22:09:29 +0000

    Change date format for events

--- config/locales/en.rb
+++ config/locales/en.rb
@@ -6,6 +6,11 @@
       formats: {
         full: ->(time, _) { "%A, #{time.day.ordinalize} %B %Y" }
       }
+    },
+    time: {
+      formats: {
+        ordinal_datetime_with_year: ->(time, _) { "#{time.day.ordinalize} %b %Y, %R" }
+      }
     }
   }
 }
