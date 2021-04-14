e37099f62087c2418397fbc1f0f6ca1a6278ebeb
Justin Malčić <j.malcic@me.com>
2021-04-14 11:54:18 +0100

    Remove helper method

--- test/helpers/spina/conferences/primer_theme/application_helper_test.rb
+++ test/helpers/spina/conferences/primer_theme/application_helper_test.rb
@@ -32,20 +32,6 @@ module Spina
           HTML
         end
 
-        test 'returns partable for parent' do
-          assert_equal [spina_page_parts(:about_page_text), spina_texts(:lipsum)], partable_for('text', parent: spina_pages(:about))
-          assert_equal [spina_structure_parts(:agm_name), spina_lines(:lipsum)], partable_for('name', parent: spina_structure_items(:agm))
-          assert_equal [spina_layout_parts(:alert), spina_lines(:lipsum)],
-                       partable_for('current_conference_alert', parent: spina_accounts(:dummy))
-          assert_equal [spina_admin_conferences_parts(:text), spina_texts(:lipsum)],
-                       partable_for('text', parent: spina_admin_conferences_conferences(:university_of_atlantis_2017))
-          Current.page = spina_pages(:about)
-          assert_equal [spina_page_parts(:about_page_text), spina_texts(:lipsum)], partable_for('text')
-          assert_raises do
-            partable_for('text', parent: 1)
-          end
-        end
-
         test 'returns new calendar' do
           assert_equal <<~ICS, calendar(name: 'Name') { concat("Lorem ipsum\r\n") }
             BEGIN:VCALENDAR\r
