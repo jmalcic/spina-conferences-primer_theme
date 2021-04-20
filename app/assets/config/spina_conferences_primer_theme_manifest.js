98757ceb8f2c9933cc04df469ee8d99bfafff619
Justin Malčić <j.malcic@me.com>
2021-04-20 18:22:22 +0100

    Fix fonts

--- app/assets/config/spina_conferences_primer_theme_manifest.js
+++ app/assets/config/spina_conferences_primer_theme_manifest.js
@@ -2,10 +2,6 @@
 //= link_directory ../javascripts/spina/conferences/primer_theme .js
 //= link spina_admin_conferences_manifest.js
 
-//= link @primer/css/fonts/Inter-Bold.woff
-//= link @primer/css/fonts/Inter-Medium.woff
-//= link @primer/css/fonts/Inter-Regular.woff
-
 //= link @github/details-dialog-element/dist/index.js
 //= link @github/include-fragment-element/dist/index.js
 //= link @github/filter-input-element/dist/index.js
