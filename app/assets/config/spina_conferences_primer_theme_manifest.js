b93d4fadaa0729894967f5be84cb3221a2097103
Louis Van Steene <louisvansteene@gmail.com>
2021-04-25 12:02:51 +0100

    Configure assets

--- app/assets/config/spina_conferences_primer_theme_manifest.js
+++ app/assets/config/spina_conferences_primer_theme_manifest.js
@@ -1,6 +1,7 @@
 //= link_directory ../stylesheets/spina/conferences/primer_theme .css
 //= link_directory ../javascripts/spina/conferences/primer_theme .js
 //= link spina_admin_conferences_manifest.js
+//= link spina_admin_journal_manifest.js
 
 //= link @github/details-dialog-element/dist/index.js
 //= link @github/include-fragment-element/dist/index.js
