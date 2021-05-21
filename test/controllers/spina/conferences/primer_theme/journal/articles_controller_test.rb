5fada8d414be85ce55c891e7ea8aad6f26df5965
Louis Van Steene <l+gh@lvs.me.uk>
2021-05-21 14:27:15 +0100

    Test article visibility

--- test/controllers/spina/conferences/primer_theme/journal/articles_controller_test.rb
+++ test/controllers/spina/conferences/primer_theme/journal/articles_controller_test.rb
@@ -11,10 +11,23 @@ module Spina
 
           setup do
             @article = spina_admin_journal_articles :new_wave
+            @draft_article = spina_admin_journal_articles :empty_article
           end
 
           test 'should get show' do
             get frontend_issue_article_url(@article.issue, @article)
+            assert_response :success
+          end
+
+          test 'should not get show for invisible article' do
+            get frontend_issue_article_url(@draft_article.issue, @draft_article)
+            assert_response :missing
+          end
+
+          test 'should get show for invisible article if authenticated' do
+            @user = spina_users :joe
+            post admin_sessions_url, params: { email: @user.email, password: 'password' }
+            get frontend_issue_article_url(@draft_article.issue, @draft_article)
             assert_response :success
           end
 
