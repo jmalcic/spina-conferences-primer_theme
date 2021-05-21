2def8e73024569dd1bdd55861b1f32d82927f3a5
Louis Van Steene <l+gh@lvs.me.uk>
2021-05-21 14:04:13 +0100

    Install new migration

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_142133) do
+ActiveRecord::Schema.define(version: 2021_05_21_130405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2021_05_12_142133) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.jsonb "json_attributes"
+    t.integer "status", default: 0, null: false
     t.index ["issue_id"], name: "index_spina_admin_journal_articles_on_issue_id"
   end
 
