b178d6a3e9b28fdc42ca89b602050d8a0424da07
Louis Van Steene <l+gh@lvs.me.uk>
2021-05-26 16:51:18 +0100

    Add integration tests for issues index

--- test/integration/spina/conferences/primer_theme/journal/issues_navigation_test.rb
+++ test/integration/spina/conferences/primer_theme/journal/issues_navigation_test.rb
@@ -10,12 +10,14 @@ module Spina
           include ::Spina::Engine.routes.url_helpers
 
           test 'view issues' do
+            get admin_logout_path
+
             get frontend_issues_path
             assert_response :success
             assert_select 'main' do
               assert_select 'div#journal-issues-list' do
                 assert_select 'ul' do
-                  Spina::Admin::Journal::Issue.sorted_desc.each do |issue|
+                  Spina::Admin::Journal::Issue.sorted_desc.where('date <= ?', Time.zone.today).each do |issue|
                     assert_select 'li' do
                       assert_select 'h3', text: I18n.t('spina.conferences.primer_theme.journal.volume_issue',
                                                       volume_number: issue.volume.number,
@@ -23,6 +25,49 @@ module Spina
                       assert_select('h4', text: issue.title) if issue.title.present?
                       assert_select 'time', text: I18n.l(issue.date, format: :long)
                     end
+                  end
+                end
+              end
+            end
+          end
+
+          test 'view issues when there are no issues' do
+            Spina::Admin::Journal::Issue.destroy_all
+            get frontend_issues_path
+            assert_response :success
+            assert_select 'div#journal-issues-list' do
+              assert_select 'h3', text: I18n.t('spina.conferences.primer_theme.journal.issues.index.no_issues')
+            end
+          end
+
+          test 'view issues when there are no past issues' do
+            get admin_logout_path
+
+            Spina::Admin::Journal::Issue.destroy_all
+            new_issue = Spina::Admin::Journal::Issue.create!(number: 1, volume: Spina::Admin::Journal::Volume.first, date: Time.zone.today + 1.day)
+            get frontend_issues_path
+            assert_response :success
+            assert_select 'div#journal-issues-list' do
+              assert_select 'h3', text: I18n.t('spina.conferences.primer_theme.journal.issues.index.no_issues')
+            end
+          end
+
+          test 'view issues as admin when there are no past issues' do
+            user = spina_users :joe
+            post admin_sessions_path, params: { email: user.email, password: 'password' }
+
+            Spina::Admin::Journal::Issue.destroy_all
+            new_issue = Spina::Admin::Journal::Issue.create!(number: 1, volume: Spina::Admin::Journal::Volume.first, date: Time.zone.today + 1.day)
+            get frontend_issues_path
+            assert_response :success
+            assert_select 'main' do
+              assert_select 'div#journal-issues-list' do
+                assert_select 'ul' do
+                  assert_select 'li' do
+                    assert_select 'h3', text: I18n.t('spina.conferences.primer_theme.journal.volume_issue',
+                                                    volume_number: new_issue.volume.number,
+                                                    issue_number: new_issue.number)
+                    assert_select 'time', text: I18n.l(new_issue.date, format: :long)
                   end
                 end
               end
