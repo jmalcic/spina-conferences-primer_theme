347d1e0c207cc5776fdb8b72e6ea212b1ae2ddfb
Louis Van Steene <louis@lvs.me.uk>
2021-09-07 19:10:30 +0100

    Fix checkbox values

--- app/models/spina/parts/conferences/primer_theme/checkbox.rb
+++ app/models/spina/parts/conferences/primer_theme/checkbox.rb
@@ -4,8 +4,8 @@ module Spina
       module PrimerTheme
         class Checkbox < Spina::Parts::Base
           # Booleans don't work nicely with Spina page parts, so we have this mess
-          CHECKED_VALUE = "Archived"
-          UNCHECKED_VALUE = "Current"
+          CHECKED_VALUE = "1"
+          UNCHECKED_VALUE = "0"
 
           attr_json :content, :string, default: UNCHECKED_VALUE
         end
